e394844c88b5571ca5251c249ff45d2051c8f157
PirataRochaPotente <vinyrocha.m@gmail.com>
2025-05-20 21:52:07 -0300

    Add files via upload

---
+++ aps_estrura_de_dados/menu_interativo/menu_interativo/Program.cs
@@ -0,0 +1,48 @@
+﻿,using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menu_interativo
+{
+    internal class Program
+    {
+        public class menu
+        {
+            public class  vector
+            {
+
+            }
+            public class array
+            {
+
+            }
+            public class list
+            {
+
+            }
+            public class queue
+            {
+
+            }
+            
+            public class stack
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int escolha = 0;
+
+            switch (escolha){
+                case 1:
+                   
+                break;
+
+            }
+        }
+    }
+}
